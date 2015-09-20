25c846ac2318d1744338954687a5a750c1c59dd9
vietpn <vietpnk53@gmail.com>
2015-09-20 12:38:51 +0700

    update

--- GruntFile.js
+++ GruntFile.js
@@ -6,18 +6,53 @@ module.exports = function(grunt){
         // Basic setting and info about our plugin
         pkg: grunt.file.readJSON("package.json"),
 
+        watch: {
+            scripts: {
+                files: ['html/js/*.js', 'html/css/*.css'],
+                tasks: ['cssmin', 'uglify']
+            },
+        },
+
         // Name of plugin (plugin name without the "grunt-contrib")
+        // sass
+        sass: {
+            dist: {
+                files: {
+                    'html/css/content.sass.css' : 'html/css/content.css'
+                }
+            }
+        },
+
+
+        // concat
+        concat: {
+            options: {
+                separator: ';',
+                stripBanners: true,
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %> */',
+            },
+            dist: {
+                src: ['html/js/toggle.js', 'html/js/toggle1.js'],
+                dest: 'html/js/main.min.js'
+            }
+        },
         // min css
         cssmin: {
             target: {
                 files: [{
-                    expand: true,
-                    cwd: 'html/css/',
-                    src: ['*.css', '*.min.css'],
-                    dest: 'html/css/',
-                    ext: '.min.css'
+                    'html/css/main.min.css' : ['html/css/content.css', 'html/css/sidebar.css']
                 }]
-            }
+            },
+            //target: {
+            //    files: [{
+            //        expand: true,
+            //        cwd: 'html/css/',
+            //        src: ['*.css', '*.min.css'],
+            //        dest: 'html/css/',
+            //        ext: '.min.css'
+            //    }]
+            //}
         },
         // min js
         uglify: {
@@ -25,12 +60,12 @@ module.exports = function(grunt){
                 mangle: false
             },
             dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'html/js/',
-                    src: ['**/*.js'],
-                    dest: 'js'
-                }],
+                //files: [{
+                //    expand: true,
+                //    cwd: 'html/js/',
+                //    src: ['**/*.js'],
+                //    dest: 'js'
+                //}],
                 files: {
                     'html/js/toggle.min.js': ['html/js/toggle.js']
                 }
@@ -40,9 +75,12 @@ module.exports = function(grunt){
 
 
     // Load the plugin
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-contrib-concat')
     grunt.loadNpmTasks('grunt-contrib-cssmin')
     grunt.loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-contrib-sass')
 
     // Do the task
-    grunt.registerTask('default', ['cssmin', 'uglify'])
+    grunt.registerTask('default', ['watch'])
 }
